1c9a71f5f41760b1777ff050b132785a5aaed7a1
Navdeep Ruhil <navdeep@scriza.in>
2024-04-29 11:41:54 +0530

    BlogApp

---
+++ src/test/java/com/spring/blogapp_Springboot/BlogappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogapp_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
